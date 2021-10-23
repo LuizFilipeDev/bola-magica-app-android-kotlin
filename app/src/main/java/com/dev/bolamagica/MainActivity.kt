a168f99ce516d0b761de40669255cc8ab9b4e67b
Luiz Filipe <74942532+LuizFilipeDev@users.noreply.github.com>
2021-10-22 22:19:19 -0300

    V1.0

---
+++ app/src/main/java/com/dev/bolamagica/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.dev.bolamagica
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    val resposta = arrayOf("Sim", "Não", "Talvez")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botao = findViewById<Button>(R.id.botaoEnviar)
+        val textoPergunta = findViewById<EditText>(R.id.textoPergunta)
+        val textoResultado = findViewById<TextView>(R.id.textoResultado)
+        var numeroRandomico: Int
+
+        botao.setOnClickListener{
+
+            numeroRandomico = Random.Default.nextInt(until = 3)
+
+            if(textoPergunta.text.isNotEmpty())
+            {
+                textoResultado.text = resposta[numeroRandomico]
+            }
+            else
+            {
+                textoResultado.text = ""
+                textoPergunta.hint = "Faça a sua pergunta"
+            }
+        }
+    }
+}
